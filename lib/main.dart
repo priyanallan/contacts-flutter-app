636d077a21a0fa265059103ea0437727008933f1
Vishnu Priya <priyav.999@gmail.com>
2018-10-12 17:15:41 +0200

    Adding MaterialApp Widget to use material design components

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(
-      Center(
-          child : Padding(
-              padding: const EdgeInsets.only(top: 24.0),
-              child: new Text("Hellow World", textDirection: TextDirection.ltr,)
-          )
-      )
+    MaterialApp(
+      title: "Flutter App",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Colors.lightGreen
+      ),
+    )
   );
 }
