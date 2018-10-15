183525232cb0d949ea4e74a7d305a005f73075d2
Vishnu Priya <priyav.999@gmail.com>
2018-10-15 16:58:05 +0200

    Modified Contacts View to extend directly from ListView

--- lib/contacts_view.dart
+++ lib/contacts_view.dart
@@ -1,19 +1,13 @@
 import 'package:flutter/material.dart';
 import 'contacts_data.dart';
 
-class _ContactsListItem extends StatelessWidget{  // Class is declared Private by underscore
-  final Contact _contact;
+class _ContactsListItem extends ListTile{  // Class is declared Private by underscore
 
-  const _ContactsListItem(this._contact) : super();
-
-  @override
-  Widget build(BuildContext context) {
-    return new ListTile(
-      leading: CircleAvatar(
-        child: Text(_contact.fullName[0]),
-      ),
-      title: Text(_contact.fullName),
-      subtitle: Text(_contact.email),
-    );
-  }
+  _ContactsListItem(Contact contact) : super(
+    title : new Text(contact.fullName),
+    subtitle : new Text(contact.email),
+    leading: CircleAvatar(
+      child: Text(contact.fullName[0]),
+    )
+  );
 }
